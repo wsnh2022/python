0d3bd51f87334a1456c854acc0898500416f2b52
wsnh2022 <99902547+wsnh2022@users.noreply.github.com>
2024-03-05 16:18:01 +0530

    python_notes.py

---
+++ python_notes.py
@@ -0,0 +1,18 @@
+# ------------------------ lesson 1 --------------------------------------------------
+# ====== Print Modifiers
+
+print('hai yogi are you excited for 100 days of coding !!!')
+
+##--- Print Modifiers with Examples 
+print('She said: "Hello" and then left.')  # Example 2 Working Code ✅
+
+# --- with Examples
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote") 
+
+#To Avoid repeated prints into next lines use backslash
+print("hai\nhai\nhai") 
+
+# This will print with an extra space before the exclamation mark.
+print("i " + 'love ' + '"you" ' + '!!!' + "\n💗\n💗\n💗")  
